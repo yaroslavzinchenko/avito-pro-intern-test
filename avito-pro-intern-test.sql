1d8308dc29e8f52f812df9a24b5bf68316bd8efc
Yaroslav Zinchenko <zin.yar@mail.ru>
2019-09-29 01:25:04 +0300

    Update avito-pro-intern-test.sql

--- avito-pro-intern-test.sql
+++ avito-pro-intern-test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 28 2019 г., 22:39
+-- Время создания: Сен 29 2019 г., 01:24
 -- Версия сервера: 8.0.15
 -- Версия PHP: 7.3.2
 
@@ -32,9 +32,23 @@ CREATE TABLE `generations` (
   `id` int(11) NOT NULL,
   `value` text NOT NULL,
   `type` varchar(100) NOT NULL,
-  `length` varchar(255) NOT NULL,
+  `length` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `generations`
+--
+
+INSERT INTO `generations` (`id`, `value`, `type`, `length`) VALUES
+(195, '{5D0B1B57-0E36-2DB7-C702-68335B7D15E5}', 'guid', NULL),
+(196, '{2EB63C24-5367-F46E-3F55-30658725B081}', 'guid', NULL),
+(197, '{2CC82F15-D7EA-7A57-6C05-AF990212BFAF}', 'guid', NULL),
+(198, '{907EADD0-003B-E26C-59C8-EB286488D361}', 'guid', NULL),
+(199, 'dAAJS39', 'custom', '7'),
+(200, 'SAd3sL3', 'custom', '7'),
+(201, 'As<S3Ls', 'custom', '7'),
+(202, '<<s13L1', 'custom', '7');
 
 --
 -- Индексы сохранённых таблиц
@@ -54,7 +68,7 @@ ALTER TABLE `generations`
 -- AUTO_INCREMENT для таблицы `generations`
 --
 ALTER TABLE `generations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=105;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=203;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
